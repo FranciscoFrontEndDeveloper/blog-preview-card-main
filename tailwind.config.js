61f57175e98d1a931042b6e2102e8ab8df89ab85
FRANCISCO <pachonetworking@gmail.com>
2023-12-29 16:07:24 -0500

    shadow finished

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,10 +9,16 @@ module.exports = {
       Black: "hsl(0, 0%, 7%)",
     },
     fontFamily: {
-      body: ["Figtree"],
+      'Figtree': ["Figtree"],
     },
     gridTemplateRows: {
       "grid-rows-2": "repeat(2,minmax(auto,1fr))",
+    },
+    boxShadow: {
+      'shadow-Black':'10px 10px 0px 0px #121212)'
+    },
+    borderRadius: {
+      'lg':'1.5rem'
     },
     extend: {},
   },
